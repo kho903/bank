1566b87167212540fc4173aae6fc734df3889d9c
Kim Ji Hun <gmldnr2222@naver.com>
2023-04-07 12:17:28 +0900

    test: 회원가입 컨트롤러 테스트

---
+++ src/test/java/shop/jikim/bank/web/UserControllerTest.java
@@ -0,0 +1,99 @@
+package shop.jikim.bank.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static shop.jikim.bank.dto.user.UserRequestDto.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.jikim.bank.config.dummy.DummyObject;
+import shop.jikim.bank.domain.user.User;
+import shop.jikim.bank.domain.user.UserRepository;
+import shop.jikim.bank.dto.user.UserRequestDto;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class UserControllerTest extends DummyObject {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private ObjectMapper om;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@BeforeTestClass
+	public void setUp() {
+		dataSetting();
+	}
+
+	private void dataSetting() {
+		userRepository.save(newUser("user", "1234"));
+	}
+
+	@Test
+	@Order(1)
+	public void join_success_test() throws Exception {
+		// given
+		JoinRequestDto joinRequestDto = new JoinRequestDto();
+		joinRequestDto.setUsername("love");
+		joinRequestDto.setPassword("1234");
+		joinRequestDto.setEmail("love@naver.com");
+		joinRequestDto.setFullname("러브");
+
+		String requestBody = om.writeValueAsString(joinRequestDto);
+		System.out.println("requestBody = " + requestBody);
+
+		// when
+		ResultActions resultActions = mvc.perform(
+			post("/api/join")
+				.content(requestBody)
+				.contentType(MediaType.APPLICATION_JSON)
+		);
+		String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+		System.out.println("responseBody = " + responseBody);
+
+		// then
+		resultActions.andExpect(status().isCreated());
+	}
+
+	@Test
+	@Order(2)
+	public void join_fail_test() throws Exception {
+		// given
+		JoinRequestDto joinRequestDto = new JoinRequestDto();
+		joinRequestDto.setUsername("love");
+		joinRequestDto.setPassword("1234");
+		joinRequestDto.setEmail("love@naver.com");
+		joinRequestDto.setFullname("러브");
+
+		String requestBody = om.writeValueAsString(joinRequestDto);
+
+		// when
+		ResultActions resultActions = mvc.perform(
+			post("/api/join")
+				.content(requestBody)
+				.contentType(MediaType.APPLICATION_JSON)
+		);
+
+		// then
+		resultActions.andExpect(status().isBadRequest());
+	}
+}
