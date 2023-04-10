c2cbfd95d664bf43584264e65e079d9ab8a55e50
Kim Ji Hun <gmldnr2222@naver.com>
2023-04-10 13:26:16 +0900

    test: unsuccessfulAuthentication 테스트

--- src/test/java/shop/jikim/bank/config/jwt/JwtAuthenticationFilterTest.java
+++ src/test/java/shop/jikim/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -11,15 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import shop.jikim.bank.config.dummy.DummyObject;
 import shop.jikim.bank.domain.user.UserRepository;
 
+@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class JwtAuthenticationFilterTest extends DummyObject {
@@ -63,5 +64,29 @@ class JwtAuthenticationFilterTest extends DummyObject {
 		assertNotNull(header);
 		assertTrue(header.startsWith(JwtVO.TOKEN_PREFIX));
 		resultActions.andExpect(jsonPath("$.data.username").value("user"));
+	}
+
+	@Test
+	public void unsuccessfulAuthentication() throws Exception {
+	    // given
+		LoginRequestDto loginRequestDto = new LoginRequestDto();
+		loginRequestDto.setUsername("");
+		loginRequestDto.setPassword("1234");
+		String requestBody = om.writeValueAsString(loginRequestDto);
+		System.out.println("requestBody = " + requestBody);
+
+		// when
+		ResultActions resultActions = mvc.perform(
+			post("/api/login")
+				.content(requestBody)
+				.contentType(MediaType.APPLICATION_JSON)
+		);
+		String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+		String header = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+		System.out.println("responseBody = " + responseBody);
+		System.out.println("header = " + header);
+
+		// then
+		resultActions.andExpect(status().isUnauthorized());
 	}
 }
