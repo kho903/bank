164f5d0d07984d5c75e1091e3d933362f3570979
Kim Ji Hun <gmldnr2222@naver.com>
2023-04-12 12:53:34 +0900

    feat: 본인계좌목록보기 서비스 만들기

--- src/main/java/shop/jikim/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/jikim/bank/domain/account/AccountRepository.java
@@ -1,5 +1,6 @@
 package shop.jikim.bank.domain.account;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	// TODO: refactoring (현재, 계좌 소유자 확인시에 쿼리가 두 번 나감. -> join fetch)
 	Optional<Account> findByNumber(Long number);
+
+	// jpa query method
+	// select * from account where user_id = :id;
+	List<Account> findByUser_Id(Long id);
 }
--- src/main/java/shop/jikim/bank/service/AccountService.java
+++ src/main/java/shop/jikim/bank/service/AccountService.java
@@ -1,11 +1,16 @@
 package shop.jikim.bank.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import shop.jikim.bank.domain.account.Account;
 import shop.jikim.bank.domain.account.AccountRepository;
 import shop.jikim.bank.domain.user.User;
@@ -41,4 +46,40 @@ public class AccountService {
 		return new AccountSaveResponseDto(accountPS);
 	}
 
+	public AccountListResponseDto findAccountByUser(Long userId) {
+		User userPs = userRepository.findById(userId)
+			.orElseThrow(() -> new CustomApiException("유저를 찾을 수 없습니다."));
+
+		// 유저의 모든 계좌목록
+		List<Account> accountListPS = accountRepository.findByUser_Id(userId);
+		return new AccountListResponseDto(userPs, accountListPS);
+	}
+
+	@Getter
+	@Setter
+	public static class AccountListResponseDto {
+		private String fullname;
+		private List<AccountDto> accounts = new ArrayList<>();
+
+		public AccountListResponseDto(User user, List<Account> accounts) {
+			this.fullname = fullname;
+			this.accounts = accounts.stream()
+				.map(AccountDto::new)
+				.collect(Collectors.toList());
+		}
+
+		@Getter
+		@Setter
+		public class AccountDto {
+			private Long id;
+			private Long number;
+			private Long balance;
+
+			public AccountDto(Account account) {
+				this.id = account.getId();
+				this.number = account.getNumber();
+				this.balance = account.getBalance();
+			}
+		}
+	}
 }
